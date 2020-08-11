f5e25f4b3fd37f074581c0f93f114bf4aed96d52
seunghui <zlxl0519@naver.com>
2020-08-11 16:34:07 +0900

    Spring05 /users/login.do 요청처리 구현

--- Spring05_Final/src/main/java/com/gura/spring05/users/service/UsersService.java
+++ Spring05_Final/src/main/java/com/gura/spring05/users/service/UsersService.java
@@ -2,9 +2,14 @@ package com.gura.spring05.users.service;
 
 import java.util.Map;
 
+import javax.servlet.http.HttpSession;
+
+import org.springframework.web.servlet.ModelAndView;
+
 import com.gura.spring05.users.dto.UsersDto;
 
 public interface UsersService {
 	public Map<String, Object> isExistId(String inputId);
 	public void addUser(UsersDto dto);
+	public void loginProcess(UsersDto dto, ModelAndView mView, HttpSession session);//mView 에는 처리 결과를 담도록
 }
