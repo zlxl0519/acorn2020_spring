a846720b7ed7569e7f7ed7b7fa9558e5a84b5154
seunghui <zlxl0519@naver.com>
2020-08-11 15:16:16 +0900

    String05/users/signup.do 요청 처리 구현됨.

--- Spring05_Final/src/main/java/com/gura/spring05/user/controller/UsersController.java
+++ Spring05_Final/src/main/java/com/gura/spring05/user/controller/UsersController.java
@@ -7,7 +7,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.ModelAndView;
 
+import com.gura.spring05.users.dto.UsersDto;
 import com.gura.spring05.users.service.UsersService;
 
 @Controller
@@ -29,5 +31,15 @@ public class UsersController {
 	public Map<String, Object> checkid(@RequestParam String inputId){//Service 에 파라미터를 인자를 통해서 보내고 Service 에서 Map 을 리턴해준다.
 		//service 가 리턴해주는 Map 객체를 리턴한다.
 		return service.isExistId(inputId);
+	}
+	
+	//회원 가입 요청 처리
+	@RequestMapping("/users/signup")
+	public ModelAndView signup(UsersDto dto, ModelAndView mView) {
+		//service 객체를 이용해서 사용자 정보를 추가 한다.
+		service.addUser(dto);
+		// view 페이지로 forward 이동해서 응답하기
+		mView.setViewName("users/signup");
+		return mView;
 	}
 }
--- Spring05_Final/src/main/java/com/gura/spring05/users/dao/UsersDaoImpl.java
+++ Spring05_Final/src/main/java/com/gura/spring05/users/dao/UsersDaoImpl.java
@@ -4,6 +4,8 @@ import org.apache.ibatis.session.SqlSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
+import com.gura.spring05.users.dto.UsersDto;
+
 @Repository
 public class UsersDaoImpl implements UsersDao{
 	@Autowired
@@ -19,6 +21,11 @@ public class UsersDaoImpl implements UsersDao{
 			return true;
 		}
 		
+	}
+
+	@Override
+	public void insert(UsersDto dto) {
+		session.insert("users.insert", dto);
 	}
 	
 }
