bc7522d8d18f3cdfc74e6ddd192858636eafd4c3
seunghui <zlxl0519@naver.com>
2020-08-11 12:25:19 +0900

    Spring05/users/signup_form.do 요청 처리

--- Spring05_Final/src/main/java/com/gura/spring05/user/controller/UsersController.java
+++ Spring05_Final/src/main/java/com/gura/spring05/user/controller/UsersController.java
@@ -2,6 +2,7 @@ package com.gura.spring05.user.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.gura.spring05.users.service.UsersService;
 
@@ -10,4 +11,11 @@ public class UsersController {
 	@Autowired
 	private UsersService service;
 	
+	//회원 가입 폼 요청 처리
+	@RequestMapping("/users/signup_form")
+	public String signupForm() {
+		
+		// /WEB-INF/views/users/signup_form.jsp 페이지로 forward 이동해서 응답
+		return "users/signup_form"; //요청처리 경로와 꼭 같게 쓸필요는 없다.(WEB-INF 안에서는 굳이 폴더를 만들지 않아도 된다.)확인하기 쉽도록 비슷하게 만든것
+	}
 }
