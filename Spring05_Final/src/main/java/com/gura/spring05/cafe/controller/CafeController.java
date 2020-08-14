e05814e9217d93eb9046cb79577d5a4051164230
seunghui <zlxl0519@naver.com>
2020-08-14 13:07:19 +0900

    Spring05 cafeDao 완성 ,cafe list 보기 구현

--- Spring05_Final/src/main/java/com/gura/spring05/cafe/controller/CafeController.java
+++ Spring05_Final/src/main/java/com/gura/spring05/cafe/controller/CafeController.java
@@ -1,7 +1,11 @@
 package com.gura.spring05.cafe.controller;
 
+import javax.servlet.http.HttpServletRequest;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 import com.gura.spring05.cafe.service.CafeService;
 
@@ -9,4 +13,11 @@ import com.gura.spring05.cafe.service.CafeService;
 public class CafeController {
 	@Autowired
 	private CafeService cafeService;
+	
+	@RequestMapping("/cafe/list")
+	public ModelAndView getList(HttpServletRequest request, ModelAndView mView) {
+		cafeService.getList(request);
+		mView.setViewName("cafe/list");
+		return mView;
+	}
 }
