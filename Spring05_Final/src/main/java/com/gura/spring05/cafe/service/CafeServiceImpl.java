e211b177508e3e02fbbf49c7cad1e5b13355b624
seunghui <zlxl0519@naver.com>
2020-08-20 14:24:17 +0900

    Spring05 에 댓글 페이징 ui 처리

--- Spring05_Final/src/main/java/com/gura/spring05/cafe/service/CafeServiceImpl.java
+++ Spring05_Final/src/main/java/com/gura/spring05/cafe/service/CafeServiceImpl.java
@@ -102,36 +102,36 @@ public class CafeServiceImpl implements CafeService{
 
 	@Override
 	public void getDetail(HttpServletRequest request) {
-		//파라미터로 전달되는 글번호
+		//파라미터로 전달되는 글번호 
 		int num=Integer.parseInt(request.getParameter("num"));
 		/*
-		검색 키워드에 관련된 처리
+		검색 키워드에 관련된 처리 
 		*/
-		String keyword=request.getParameter("keyword");
-		//어떤 것으로 검색했는지 파라미터 값
-		String condition=request.getParameter("condition");
-		if(keyword==null){//전달된 키워드가 없다면
+		String keyword=request.getParameter("keyword"); //검색 키워드
+		String condition=request.getParameter("condition"); //검색 조건
+		if(keyword==null){//전달된 키워드가 없다면 
+			keyword=""; //빈 문자열을 넣어준다. 
 			condition="";
-			keyword="";//빈 문자열을 넣어준다.
 		}
-		//인코딩된 키워드를 미리 만들어 둔다.
+		//인코딩된 키워드를 미리 만들어 둔다. 
 		String encodedK=URLEncoder.encode(keyword);
-		//글번호와 건색 키워드를 담을 CafeDto 객체 생성
-		CafeDto dto=new CafeDto();
-		dto.setNum(num);//글번호 담기
 		
-		if(!keyword.equals("")){//만일 키워드가 넘어온다면
+		//글번호와 검색 키워드를 담을 CafeDto 객체 생성
+		CafeDto dto=new CafeDto();
+		dto.setNum(num);//글번호 담기 
+		
+		if(!keyword.equals("")){ //만일 키워드가 넘어온다면 
 			if(condition.equals("title_content")){
-				//검색 키워드를 FileDto 객체의 필드에 담는다.
+				//검색 키워드를 FileDto 객체의 필드에 담는다. 
 				dto.setTitle(keyword);
-				dto.setContent(keyword);
+				dto.setContent(keyword);	
 			}else if(condition.equals("title")){
 				dto.setTitle(keyword);
 			}else if(condition.equals("writer")){
 				dto.setWriter(keyword);
 			}
 		}
-		//자세히 보여줄 글 정보
+		//자세히 보여줄 글 정보 
 		CafeDto resultDto=cafeDao.getData(dto);
 		
 		//view 페이지에서 필요한 내용 HttpServletRequest 에 담기
@@ -143,19 +143,39 @@ public class CafeServiceImpl implements CafeService{
 		//글 조회수 올리기
 		cafeDao.addViewCount(num);
 		
-		//보여줄 페이지의 번호
-		int pageNum=1;
+		/* 아래는 댓글 페이징 처리 관련 비즈니스 로직 입니다.*/
+		final int PAGE_ROW_COUNT=5;
+		final int PAGE_DISPLAY_COUNT=5;
 		
+		
+		//전체 row 의 갯수를 읽어온다.
+		//자세히 보여줄 글의 번호가 ref_group  번호 이다. 
+		int totalRow=cafeCommentDao.getCount(num);
+
+		//보여줄 페이지의 번호(만일 pageNum 이 넘어오지 않으면 가장 마지막 페이지)
+		String strPageNum=request.getParameter("pageNum");
+		//전체 페이지의 갯수 구하기
+		int totalPageCount=
+						(int)Math.ceil(totalRow/(double)PAGE_ROW_COUNT);
+		int pageNum=totalPageCount;
+		if(strPageNum!=null) {
+			pageNum=Integer.parseInt(strPageNum);
+		}
 		//보여줄 페이지 데이터의 시작 ResultSet row 번호
 		int startRowNum=1+(pageNum-1)*PAGE_ROW_COUNT;
 		//보여줄 페이지 데이터의 끝 ResultSet row 번호
 		int endRowNum=pageNum*PAGE_ROW_COUNT;
 		
-		//전체 row 의 갯수를 읽어온다.
-		int totalRow=cafeCommentDao.getCount(num);
-		//전체 페이지의 갯수 구하기
-		int totalPageCount=
-				(int)Math.ceil(totalRow/(double)PAGE_ROW_COUNT);
+		
+		//시작 페이지 번호
+		int startPageNum=
+			1+((pageNum-1)/PAGE_DISPLAY_COUNT)*PAGE_DISPLAY_COUNT;
+		//끝 페이지 번호
+		int endPageNum=startPageNum+PAGE_DISPLAY_COUNT-1;
+		//끝 페이지 번호가 잘못된 값이라면 
+		if(totalPageCount < endPageNum){
+			endPageNum=totalPageCount; //보정해준다. 
+		}
 		
 		// CafeCommentDto 객체에 위에서 계산된 startRowNum 과 endRowNum 을 담는다.
 		CafeCommentDto commentDto=new CafeCommentDto();
@@ -163,12 +183,15 @@ public class CafeServiceImpl implements CafeService{
 		commentDto.setEndRowNum(endRowNum);
 		//ref_group 번호도 담는다.
 		commentDto.setRef_group(num);
-
+		
 		//DB 에서 댓글 목록을 얻어온다.
 		List<CafeCommentDto> commentList=cafeCommentDao.getList(commentDto);
 		//request 에 담아준다.
 		request.setAttribute("commentList", commentList);
 		request.setAttribute("totalPageCount", totalPageCount);
+		request.setAttribute("startPageNum", startPageNum);
+		request.setAttribute("endPageNum", endPageNum);
+		request.setAttribute("pageNum", pageNum);
 	}
 
 	@Override
