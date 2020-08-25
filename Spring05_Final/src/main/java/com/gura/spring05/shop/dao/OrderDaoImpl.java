b21a6f1de2775b32e81a75f4a14ac7663d32cc9c
seunghui <zlxl0519@naver.com>
2020-08-25 10:21:32 +0900

    Spring05 ShopServiceImpl 구입 기능 구현중... ,DataAccessException 상속받은 익셉션클래스만듬

--- Spring05_Final/src/main/java/com/gura/spring05/shop/dao/OrderDaoImpl.java
+++ Spring05_Final/src/main/java/com/gura/spring05/shop/dao/OrderDaoImpl.java
@@ -11,7 +11,7 @@ public class OrderDaoImpl implements OrderDao{
 	
 	@Autowired
 	private SqlSession session;
-
+	//상품 주문 정보를 저장하는 메소드
 	@Override
 	public void addOrder(OrderDto dto) {
 		session.insert("shop.addOrder", dto);
--- Spring05_Final/src/main/java/com/gura/spring05/shop/dao/ShopDaoImpl.java
+++ Spring05_Final/src/main/java/com/gura/spring05/shop/dao/ShopDaoImpl.java
@@ -12,28 +12,32 @@ import com.gura.spring05.shop.dto.ShopDto;
 public class ShopDaoImpl implements ShopDao{
 	@Autowired
 	private SqlSession session;
-
+	
+	//public void setSession(SqlSession session){
+	//	this.session = session;
+	//} 위에 @Autowired 는 이런 형식으로 생성되서 들어가는 것
+	
 	@Override
 	public List<ShopDto> getList() {
 		
 		return session.selectList("shop.getList");
 	}
-
+	//재고의 갯수를 1 줄이기
 	@Override
 	public void minusCount(int num) {
 		session.update("shop.minusCount", num);
 	}
-
+	//계좌 잔고 줄이기
 	@Override
 	public void minusMoney(ShopDto dto) {
 		session.update("shop.minusMoney", dto);
 	}
-
+	//상품 구입 가격의 10% 를 포인트로 적립하는 메소드
 	@Override
 	public void plusPoint(ShopDto dto) {
 		session.update("shop.plusPoint", dto);
 	}
-
+	//상품 번호에 해당하는 상품의 가격을 리턴하는 메소드
 	@Override
 	public int getPrice(int num) {
 	
